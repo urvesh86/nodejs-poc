2e369dd9547d8e80ae0ee45385cce3cfc68a908d
Urveshp <urvesh.purohit@coreiot.com>
2022-07-15 11:49:17 +0800

    dsfsd

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const data = [
     { name: 'urvesh', mobile: '9574399866' },
-    { name: 'Nam', mobile: '8866812157' }
+    { name: 'Nam', mobile: '8866812157' },
+    { name: 'Prans', mobile: '88665277' }
 ];
 
 module.exports = data 
